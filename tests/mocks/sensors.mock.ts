69bed41c060392d353639e5374aea0a228767923
gabrielsilper <gabrielsilper.main@gmail.com>
2026-03-20 01:14:12 -0400

    test: criado teste para a função addSensor do SensorService

--- tests/mocks/sensors.mock.ts
+++ tests/mocks/sensors.mock.ts
@@ -1,28 +1,42 @@
 import Area from "../../src/entities/Area";
 import { Sensor } from "../../src/entities/Sensor";
 
-export const areaMock : Area = {
-    id: 'a6ec24dc-eb2b-4f9a-b98c-1e4d074e2589',
-    nome: 'Area X',
-    bioma: 'Bioma X',
-    latitude: -3.065646,
-    longitude: -60.064381,
-    largura: 1000,
-    comprimento: 450,
-    sensores: []
-}
+export const areaMock: Area = {
+  id: "a6ec24dc-eb2b-4f9a-b98c-1e4d074e2589",
+  nome: "Area X",
+  bioma: "Bioma X",
+  latitude: -3.065646,
+  longitude: -60.064381,
+  largura: 1000,
+  comprimento: 450,
+  sensores: [],
+};
 
-export const sensorMock : Sensor = {
-    id: '2f19dfa2-ddbd-4f74-9b5f-fee3467e64bf',
-    serialNumber: '00001',
-    fabricante: 'Fabricante X',
-    modelo: 'Modelo X',
-    tipo: 'Tipo X',
-    status: 'Ativo',
-    dataInstalacao: new Date(1773978173000),
-    cicloLeitura: 23,
-    latitude: -3.065604,
-    longitude: -60.064461,
-    area: areaMock,
-    leituras: [],
-}
+export const sensorMock: Sensor = {
+  id: "2f19dfa2-ddbd-4f74-9b5f-fee3467e64bf",
+  serialNumber: "00001",
+  fabricante: "Fabricante X",
+  modelo: "Modelo X",
+  tipo: "Tipo X",
+  status: "Ativo",
+  dataInstalacao: new Date(1773978173000),
+  cicloLeitura: 23,
+  latitude: -3.065604,
+  longitude: -60.064461,
+  area: areaMock,
+  leituras: [],
+};
+
+export const sensorCreateDataMock = {
+  serialNumber: "00001",
+  fabricante: "Fabricante X",
+  modelo: "Modelo X",
+  tipo: "Tipo X",
+  status: "Ativo",
+  dataInstalacao: new Date(1773978173000),
+  cicloLeitura: 23,
+  latitude: -3.065604,
+  longitude: -60.064461,
+  area: areaMock,
+  leituras: [],
+};
--- tests/services/SensorService.test.ts
+++ tests/services/SensorService.test.ts
@@ -1,36 +1,94 @@
-import { vi, describe, it, expect, beforeEach } from 'vitest';
+import { vi, describe, it, expect, beforeEach } from "vitest";
 
 vi.resetModules();
 
 const mockSensorRepository = {
   find: vi.fn(),
+  findOne: vi.fn(),
+  create: vi.fn(),
+  save: vi.fn(),
 };
 
-vi.mock('../../src/database/appDataSource.js', () => {
+const mockAreaRepository = {
+  findOne: vi.fn(),
+};
+
+vi.mock("../../src/database/appDataSource.js", () => {
   return {
     appDataSource: {
-      getRepository: () => mockSensorRepository,
+      getRepository: (entity: any) => {
+        if (entity === Sensor) return mockSensorRepository;
+        if (entity === Area) return mockAreaRepository;
+
+        return {};
+      },
     },
   };
 });
 
-import SensorService from '../../src/services/SensorService';
-import { sensorMock } from '../mocks/sensors.mock';
+import SensorService from "../../src/services/SensorService";
+import {
+  areaMock,
+  sensorCreateDataMock,
+  sensorMock,
+} from "../mocks/sensors.mock";
+import { Sensor } from "../../src/entities/Sensor";
+import Area from "../../src/entities/Area";
+import { AppError } from "../../src/errors/AppError";
 
-describe('SensorService testes', () => {
+describe("SensorService testes", () => {
   const service = new SensorService();
 
   beforeEach(() => {
     vi.clearAllMocks();
   });
 
-  it('Deve retornar todos os sensores', async () => {
+  it("Deve retornar todos os sensores", async () => {
+    // Arrange
     const sensorsMock = [sensorMock];
-
     mockSensorRepository.find.mockResolvedValue(sensorsMock);
 
+    // Act
     const result = await service.getAllSensors();
 
+    // Assert
     expect(result).toEqual(sensorsMock);
+  });
+
+  it("Deve criar um sensor com sucesso", async () => {
+    // Arrange
+    mockSensorRepository.findOne.mockResolvedValue(null);
+    mockAreaRepository.findOne.mockResolvedValue(areaMock);
+    mockSensorRepository.create.mockResolvedValue(sensorMock);
+    mockSensorRepository.save.mockResolvedValue(sensorMock);
+
+    // Act
+    const sensorCreated = await service.addSensor(sensorCreateDataMock);
+
+    // Assert
+    expect(sensorCreated).toHaveProperty("id");
+    expect(sensorCreated.id).toBe(sensorMock.id);
+    expect(sensorCreated).toEqual(sensorMock);
+  });
+
+  it("Deve lançar um erro ao criar um sensor com serialNumber que já existe", async () => {
+    // Arrange
+    mockSensorRepository.findOne.mockResolvedValue(sensorMock);
+
+    // Act & Assert
+    try {
+      await service.addSensor({ serialNumber: "123", area_id: 1 });
+    } catch (error: any) {
+      expect(error).toBeInstanceOf(AppError);
+      expect(error.statusCode).toBe(400);
+      expect(error.message).toBe(
+        "Sensor com este Serial Number já cadastrado!",
+      );
+
+      expect(mockSensorRepository.findOne).toHaveBeenCalled();
+      expect(mockAreaRepository.findOne).not.toHaveBeenCalled();
+      expect(mockSensorRepository.create).not.toHaveBeenCalled();
+      expect(mockSensorRepository.save).not.toHaveBeenCalled();
+    }
   });
 });
