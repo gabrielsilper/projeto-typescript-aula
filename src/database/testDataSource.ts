991f2d7f7c1ef5636e971f391bc8c2d3420a353f
gabrielsilper <gabrielsilper.main@gmail.com>
2026-03-21 20:45:01 -0400

    tests: adição do sqlite para testes com banco em memória e criação do testDataSource

---
+++ src/database/testDataSource.ts
@@ -0,0 +1,15 @@
+import { DataSource } from "typeorm";
+import Pesquisador from "../entities/Pesquisador.js";
+import RefreshToken from "../entities/RefreshToken.js";
+import Area from "../entities/Area.js";
+import { Sensor } from "../entities/Sensor.js";
+import Leitura from "../entities/Leitura.js";
+
+export const testDataSource = new DataSource({
+  type: "sqlite",
+  database: ":memory:",
+  dropSchema: true,
+  entities: [Pesquisador],
+  synchronize: true,
+  logging: false,
+});
